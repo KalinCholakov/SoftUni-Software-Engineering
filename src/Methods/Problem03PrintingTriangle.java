154852350430c5def746f5dbf5fafd56b6841d8d
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-06-17 15:49:51 +0300

    Add Task 3

---
+++ src/Methods/Problem03PrintingTriangle.java
@@ -0,0 +1,28 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Problem03PrintingTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = Integer.parseInt(scanner.nextLine());
+
+        printTriangle(input);
+    }
+
+    public static void printTriangle(int num) {
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        for (int i = num - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
