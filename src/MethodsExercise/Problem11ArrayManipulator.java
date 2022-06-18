f6e55ed638bbb25ada91645defecb1570ad548d7
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-06-19 00:42:07 +0300

    Add Task 10 to Method - Exercises

---
+++ src/MethodsExercise/Problem11ArrayManipulator.java
@@ -0,0 +1,11 @@
+package MethodsExercise;
+
+import java.util.Scanner;
+
+public class Problem11ArrayManipulator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
