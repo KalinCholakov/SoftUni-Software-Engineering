67aaf37398fafe6631c43e35d52e581f365674db
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-06-28 12:00:13 +0300

    Add Task 09 Lists Exercises

---
+++ src/ListsExercises/Problem10SoftUniCoursePlanning.java
@@ -0,0 +1,11 @@
+package ListsExercises;
+
+import java.util.Scanner;
+
+public class Problem10SoftUniCoursePlanning {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
