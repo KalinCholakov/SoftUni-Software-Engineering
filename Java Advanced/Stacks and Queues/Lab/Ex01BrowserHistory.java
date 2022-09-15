362845ad7ac77fd891cb2608c11fe670bc859d5d
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-09-15 10:32:43 +0300

    Create Ex01BrowserHistory.java

---
+++ Java Advanced/Stacks and Queues/Lab/Ex01BrowserHistory.java
@@ -0,0 +1,38 @@
+package stacksAndQueues.lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Ex01BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String currentUrl = null;
+
+        ArrayDeque<String> history = new ArrayDeque<>();
+
+        while (!input.equals("Home")) {
+
+            if (input.equals("back")) {
+                if (history.isEmpty()) {
+                    System.out.println("no previous URLs");
+                    input = scanner.nextLine();
+                    continue;
+                } else {
+                    currentUrl = history.pop();
+                }
+            } else {
+                if (currentUrl != null) {
+                    history.push(currentUrl);
+                }
+                currentUrl = input;
+            }
+
+            System.out.println(currentUrl);
+
+            input = scanner.nextLine();
+        }
+    }
+}
