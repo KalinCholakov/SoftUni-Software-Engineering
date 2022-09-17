9d2ef4ec92365afb30ce54115e861991d7679f9f
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-09-17 23:37:20 +0300

    Create Ex06RecursiveFibonacci.java

---
+++ Java Advanced/Stacks and Queues/Exercises/Ex06RecursiveFibonacci.java
@@ -0,0 +1,26 @@
+package stacksAndQueues.exercises;
+
+import java.util.Scanner;
+
+public class Ex06RecursiveFibonacci {
+    public static long[] memory;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        memory = new long[n + 1];
+        long result = fib(n);
+        System.out.println(result);
+    }
+
+    public static long fib(int n) {
+        if (n < 2) {
+            return 1;
+        }
+        if (memory[n] != 0) {
+            return memory[n];
+        }
+        memory[n] = fib(n - 1) + fib(n - 2);
+        return memory[n];
+    }
+}
