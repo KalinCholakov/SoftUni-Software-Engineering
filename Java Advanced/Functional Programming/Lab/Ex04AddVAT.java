7ba9c8055106c51c5762643f8134977ebc9929d3
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-06 21:33:34 +0300

    Create Ex04AddVAT.java

---
+++ Java Advanced/Functional Programming/Lab/Ex04AddVAT.java
@@ -0,0 +1,29 @@
+package functionalProgramming.lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.DoubleUnaryOperator;
+import java.util.stream.Collectors;
+
+public class Ex04AddVAT {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[] prices = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToDouble(Double::parseDouble)
+                .toArray();
+
+        DoubleUnaryOperator addVat = p -> p * 1.20;
+
+        Consumer<double[]> printPrices = p -> {
+            System.out.println("Prices with VAT:");
+            System.out.println(Arrays.stream(p)
+                    .map(addVat)
+                    .mapToObj(d -> String.format("%.2f", d))
+                    .collect(Collectors.joining(System.lineSeparator())));
+        };
+
+        printPrices.accept(prices);
+    }
+}
