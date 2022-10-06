eefa6e490a7181fe833e59191fe41a56a7bf9de2
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-06 21:01:14 +0300

    Create Ex02SumNumbers.java

---
+++ Java Advanced/Functional Programming/Lab/Ex02SumNumbers.java
@@ -0,0 +1,19 @@
+package functionalProgramming.lab;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class Ex02SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Function<int[], String> count = elements -> "Count = " + Arrays.stream(elements).count();
+        Function<int[], String> sum = elements -> "Sum = " + Arrays.stream(elements).sum();
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(count.apply(numbers));
+        System.out.println(sum.apply(numbers));
+    }
+}
