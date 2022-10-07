2d5f98ed6e0193fb296f3a98ad318e684b49a1e6
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-07 22:49:28 +0300

    Create Ex02KnightsOfHonor.java

---
+++ Java Advanced/Functional Programming/Exercises/Ex02KnightsOfHonor.java
@@ -0,0 +1,20 @@
+package functionalProgramming.exercises;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class Ex02KnightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+
+        Consumer<String> consumer = e -> System.out.printf("Sir %s%n", e);
+
+        names.forEach(consumer);
+    }
+}
