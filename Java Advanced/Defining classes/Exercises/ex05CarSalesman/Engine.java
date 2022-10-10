1c91d1a8c59800abc439bad18e7549229bc445ea
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-11 02:09:11 +0300

    Create Engine.java

---
+++ Java Advanced/Defining classes/Exercises/ex05CarSalesman/Engine.java
@@ -0,0 +1,35 @@
+package definingClasses.exercises.ex05CarSalesman;
+
+public class Engine {
+    String model;
+    int power;
+    String  displacement;
+    String efficiency;
+
+    public Engine(String model, int power, String displacement, String efficiency) {
+        this.model = model;
+        this.power = power;
+        this.displacement = displacement;
+        this.efficiency = efficiency;
+    }
+
+    public Engine(String model, int power) {
+        this(model, power, "n/a", "n/a");
+    }
+    
+    public String getModel() {
+        return model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getDisplacement() {
+        return displacement;
+    }
+
+    public String getEfficiency() {
+        return efficiency;
+    }
+}
