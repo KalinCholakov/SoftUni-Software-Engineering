925a7cd38691bd8841b83390d18bb0a9e6050666
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-10 23:19:02 +0300

    Create Cargo.java

---
+++ Java Advanced/Defining classes/Exercises/ex04RawData/Cargo.java
@@ -0,0 +1,15 @@
+package definingClasses.exercises.ex04RawData;
+
+public class Cargo {
+    private int cargoWeight;
+    private String cargoType;
+
+    public Cargo(int cargoWeight, String cargoType) {
+        this.cargoWeight = cargoWeight;
+        this.cargoType = cargoType;
+    }
+
+    public String getCargoType() {
+        return cargoType;
+    }
+}
