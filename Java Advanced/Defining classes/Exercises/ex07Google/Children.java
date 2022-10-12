949d8fb952161563535d1df0837a1e83c7c985fb
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-12 22:58:55 +0300

    Create Children.java

---
+++ Java Advanced/Defining classes/Exercises/ex07Google/Children.java
@@ -0,0 +1,16 @@
+package definingClasses.exercises.ex07Google;
+
+public class Children {
+    private String name;
+    private String birthday;
+
+    public Children(String name, String birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", name, birthday);
+    }
+}
