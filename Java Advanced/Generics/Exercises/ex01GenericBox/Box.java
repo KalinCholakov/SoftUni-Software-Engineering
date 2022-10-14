9431e10319a42b22873fa96ce545f650a21b2c12
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-14 13:43:13 +0300

    Create Box.java

---
+++ Java Advanced/Generics/Exercises/ex01GenericBox/Box.java
@@ -0,0 +1,25 @@
+package generics.exercises.ex01GenericBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    private List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        values.add(element);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T value : values) {
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value));
+        }
+        return sb.toString();
+    }
+}
