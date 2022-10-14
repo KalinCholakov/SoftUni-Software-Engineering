21b98e881eeb7495b842a829c23785672fa320d0
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-14 14:39:19 +0300

    Create Box.java

---
+++ Java Advanced/Generics/Exercises/ex03GenericSwapMethodString/Box.java
@@ -0,0 +1,30 @@
+package generics.exercises.ex03GenericSwapMethodString;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Box<T> {
+    private List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        values.add(element);
+    }
+
+    public void swap(int first, int second) {
+        Collections.swap(values, first, second);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T value : values) {
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value));
+        }
+        return sb.toString();
+    }
+}
