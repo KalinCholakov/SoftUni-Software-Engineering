409fa32da9ddfd75c0f4cf19f4a82175710747bb
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-30 10:27:19 +0200

    Create Main.java

---
+++ Java OOP/Working with Abstraction/Exercises/ex01CardSuit/Main.java
@@ -0,0 +1,12 @@
+package workingWithAbstraction.exercises.ex01CardSuit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Card Suits:");
+
+        for (CardSuit value : CardSuit.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", value.ordinal(), value.name());
+        }
+    }
+}
