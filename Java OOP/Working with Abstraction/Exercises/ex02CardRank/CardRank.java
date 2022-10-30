2a8395b1ab3a6e4b045acac11cc149ae46124715
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-30 11:15:24 +0200

    Create CardRank.java

---
+++ Java OOP/Working with Abstraction/Exercises/ex02CardRank/CardRank.java
@@ -0,0 +1,17 @@
+package workingWithAbstraction.exercises.ex02CardRank;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
