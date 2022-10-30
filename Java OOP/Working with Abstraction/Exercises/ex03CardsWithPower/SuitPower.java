d0186983f4e88520b49c6a2f801764de72bfb0ea
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-30 11:42:30 +0200

    Create SuitPower.java

---
+++ Java OOP/Working with Abstraction/Exercises/ex03CardsWithPower/SuitPower.java
@@ -0,0 +1,18 @@
+package workingWithAbstraction.exercises.ex03CardsWithPower;
+
+public enum SuitPower {
+    CLUBS(0),
+    DIAMONDS(13),
+    HEARTS(26),
+    SPADES(39);
+
+    private int power;
+
+    SuitPower(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+}
