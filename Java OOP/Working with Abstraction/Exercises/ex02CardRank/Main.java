fd86905c66b013f66e5cb670ed4bb60dc4a95b6f
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-30 11:15:30 +0200

    Create Main.java

---
+++ Java OOP/Working with Abstraction/Exercises/ex02CardRank/Main.java
@@ -0,0 +1,12 @@
+package workingWithAbstraction.exercises.ex02CardRank;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Card Ranks:");
+
+        for (CardRank value : CardRank.values()) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", value.ordinal(), value.name());
+        }
+    }
+}
