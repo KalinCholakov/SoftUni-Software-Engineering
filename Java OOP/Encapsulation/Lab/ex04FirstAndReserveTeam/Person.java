b0ca6d9692f11f4ee26f9413ebfa356c1f884477
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-31 13:57:48 +0200

    Create Person.java

---
+++ Java OOP/Encapsulation/Lab/ex04FirstAndReserveTeam/Person.java
@@ -0,0 +1,18 @@
+package encapsulation.lab.ex04FirstAndReserveTeam;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
