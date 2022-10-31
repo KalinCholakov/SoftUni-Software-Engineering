f25d1bb53bc1d2e89eedbc91b68ef0c25e898c6e
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-10-31 13:57:52 +0200

    Create Team.java

---
+++ Java OOP/Encapsulation/Lab/ex04FirstAndReserveTeam/Team.java
@@ -0,0 +1,42 @@
+package encapsulation.lab.ex04FirstAndReserveTeam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(String name) {
+        setName(name);
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addPlayer(Person person) {
+
+        if (person.getAge() < 40) {
+            firstTeam.add(person);
+        } else {
+            reserveTeam.add(person);
+        }
+    }
+
+    public List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(firstTeam);
+    }
+
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(reserveTeam);
+    }
+}
