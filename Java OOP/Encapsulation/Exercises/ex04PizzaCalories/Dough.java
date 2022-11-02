f38b614cf23335d22d6b8deee26d3ff859b31394
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-02 18:40:08 +0200

    Create Dough.java

---
+++ Java OOP/Encapsulation/Exercises/ex04PizzaCalories/Dough.java
@@ -0,0 +1,71 @@
+package encapsulation.exercises.ex04PizzaCalories;
+
+public class Dough {
+    private String flourType;
+    private String bakingTechnique;
+    private double weight;
+
+    public Dough(String flourType, String bakingTechnique, double weight) {
+        setFlourType(flourType);
+        setBakingTechnique(bakingTechnique);
+        setWeight(weight);
+    }
+
+    private void setFlourType(String flourType) {
+        switch (flourType) {
+            case "White":
+            case "Wholegrain":
+                this.flourType = flourType;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid type of dough.");
+        }
+    }
+
+    private void setBakingTechnique(String bakingTechnique) {
+        switch (bakingTechnique) {
+            case "Crispy":
+            case "Chewy":
+            case "Homemade":
+                this.bakingTechnique = bakingTechnique;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid type of dough.");
+        }
+    }
+
+    private void setWeight(double weight) {
+        if (weight < 1 || weight > 200) {
+            throw new IllegalArgumentException("Dough weight should be in the range [1..200].");
+        }
+        this.weight = weight;
+    }
+
+    public double calculateCalories() {
+        return 2 * weight * getModifierFlourType() * getModifierBakingTechnique();
+    }
+
+    private double getModifierFlourType() {
+        switch (flourType) {
+            case "White":
+                return 1.5;
+            case "Wholegrain":
+                return 1.0;
+            default:
+                return 0;
+        }
+    }
+
+    private double getModifierBakingTechnique() {
+        switch (bakingTechnique) {
+            case "Crispy":
+                return 0.9;
+            case "Chewy":
+                return 1.1;
+            case "Homemade":
+                return 1.0;
+            default:
+                return 0;
+        }
+    }
+}
