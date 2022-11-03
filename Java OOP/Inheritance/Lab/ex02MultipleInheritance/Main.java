5b575e7e9a76f193fb3e076338a0914857c666ba
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-03 17:16:26 +0200

    Create Main.java

---
+++ Java OOP/Inheritance/Lab/ex02MultipleInheritance/Main.java
@@ -0,0 +1,12 @@
+package inheritance.lab.ex02MultipleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Puppy puppy = new Puppy();
+
+        puppy.eat();
+        puppy.bark();
+        puppy.weep();
+    }
+}
