410ffa184dca96ed2414f31a3b2ba22bdc319346
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 15:16:41 +0200

    Create Motorcycle.java

---
+++ Java OOP/Inheritance/Exercises/ex04NeedForSpeed/Motorcycle.java
@@ -0,0 +1,7 @@
+package inheritance.exercises.ex04NeedForSpeed;
+
+public class Motorcycle extends Vehicle {
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
