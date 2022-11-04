208452b74fc736d1433e322942499d6b1039b7b8
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 12:38:50 +0200

    Create DarkKnight.java

---
+++ Java OOP/Inheritance/Exercises/ex03PlayersAndMonsters/DarkKnight.java
@@ -0,0 +1,7 @@
+package inheritance.exercises.ex03PlayersAndMonsters;
+
+public class DarkKnight extends Knight {
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+}
