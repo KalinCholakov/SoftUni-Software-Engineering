57be3cc384ba6e978d207cfd5bb0d6daacff8cf2
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 12:38:36 +0200

    Create DarkWizard.java

---
+++ Java OOP/Inheritance/Exercises/ex03PlayersAndMonsters/DarkWizard.java
@@ -0,0 +1,7 @@
+package inheritance.exercises.ex03PlayersAndMonsters;
+
+public class DarkWizard extends Wizard {
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
