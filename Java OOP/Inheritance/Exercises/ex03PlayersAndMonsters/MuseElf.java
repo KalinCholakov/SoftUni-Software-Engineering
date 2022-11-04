dc0983add22c7e0f5218089da6815dad70f8286e
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 12:38:04 +0200

    Create MuseElf.java

---
+++ Java OOP/Inheritance/Exercises/ex03PlayersAndMonsters/MuseElf.java
@@ -0,0 +1,7 @@
+package inheritance.exercises.ex03PlayersAndMonsters;
+
+public class MuseElf extends Elf {
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+}
