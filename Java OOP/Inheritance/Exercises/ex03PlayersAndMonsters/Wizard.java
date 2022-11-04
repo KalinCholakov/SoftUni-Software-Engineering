3c369f1cde941e58af51a1082815d535fe27b42e
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 12:38:32 +0200

    Create Wizard.java

---
+++ Java OOP/Inheritance/Exercises/ex03PlayersAndMonsters/Wizard.java
@@ -0,0 +1,7 @@
+package inheritance.exercises.ex03PlayersAndMonsters;
+
+public class Wizard extends Hero {
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+}
