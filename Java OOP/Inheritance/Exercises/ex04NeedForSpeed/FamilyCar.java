4b07dbb2b2151f86cd62aa77cc1333301f1873db
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 15:17:01 +0200

    Create FamilyCar.java

---
+++ Java OOP/Inheritance/Exercises/ex04NeedForSpeed/FamilyCar.java
@@ -0,0 +1,8 @@
+package inheritance.exercises.ex04NeedForSpeed;
+
+public class FamilyCar extends Car {
+
+    public FamilyCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
