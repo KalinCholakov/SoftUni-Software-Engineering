8d23abbf32e8de4d99ea80ad1f0b9174e4bff505
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 12:22:12 +0200

    Create Animal.java

---
+++ Java OOP/Inheritance/Exercises/ex02Zoo/Animal.java
@@ -0,0 +1,13 @@
+package inheritance.exercises.ex02Zoo;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
