3eaed6ea7f35fb7d923e86e19670c1f445a870fb
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 15:16:51 +0200

    Create CrossMotorcycle.java

---
+++ Java OOP/Inheritance/Exercises/ex04NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,8 @@
+package inheritance.exercises.ex04NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle {
+
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
