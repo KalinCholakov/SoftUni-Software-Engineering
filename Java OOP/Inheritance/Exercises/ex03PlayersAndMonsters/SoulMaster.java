268106a53b36b669b029015c79ebea2310e693b1
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 12:38:44 +0200

    Create SoulMaster.java

---
+++ Java OOP/Inheritance/Exercises/ex03PlayersAndMonsters/SoulMaster.java
@@ -0,0 +1,7 @@
+package inheritance.exercises.ex03PlayersAndMonsters;
+
+public class SoulMaster extends DarkWizard {
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
