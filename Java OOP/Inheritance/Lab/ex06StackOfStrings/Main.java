1b6c45f48e5d23db53711f2192888081631880c6
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-04 10:47:39 +0200

    Create Main.java

---
+++ Java OOP/Inheritance/Lab/ex06StackOfStrings/Main.java
@@ -0,0 +1,19 @@
+package inheritance.lab.ex06StackOfStrings;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StackOfStrings stackOfStrings = new StackOfStrings();
+
+        stackOfStrings.push("one");
+        stackOfStrings.push("two");
+        stackOfStrings.push("three");
+
+        System.out.println(stackOfStrings.isEmpty());
+        System.out.println(stackOfStrings.peek());
+
+        System.out.println(stackOfStrings.pop());
+        System.out.println(stackOfStrings.pop());
+        System.out.println(stackOfStrings.pop());
+    }
+}
