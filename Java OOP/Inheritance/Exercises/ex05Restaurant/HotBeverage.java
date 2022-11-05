92282fae8c57650a2b10f073c0842c637a8b82d2
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-05 18:53:37 +0200

    Create HotBeverage.java

---
+++ Java OOP/Inheritance/Exercises/ex05Restaurant/HotBeverage.java
@@ -0,0 +1,10 @@
+package inheritance.exercises.ex05Restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage {
+
+    public HotBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
