c1bb3cda5754b29fd8f886ceaf7af9f6e49d8bc6
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-05 22:32:01 +0200

    Create Dog.java

---
+++ Java OOP/Inheritance/Exercises/ex06Animals/Dog.java
@@ -0,0 +1,12 @@
+package inheritance.exercises.ex06Animals;
+
+public class Dog extends Animal {
+
+    public Dog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    public String produceSound() {
+        return "Woof!";
+    }
+}
