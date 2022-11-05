5a1c7466a400fd40ed0d4b1abeb00a55de4916d1
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-05 18:54:05 +0200

    Create Salmon.java

---
+++ Java OOP/Inheritance/Exercises/ex05Restaurant/Salmon.java
@@ -0,0 +1,11 @@
+package inheritance.exercises.ex05Restaurant;
+
+import java.math.BigDecimal;
+
+public class Salmon extends MainDish {
+    public static final double SALMON_GRAMS = 22;
+
+    public Salmon(String name, BigDecimal price) {
+        super(name, price, SALMON_GRAMS);
+    }
+}
