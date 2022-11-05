1aae25c1c0827319d0a57eed119b33ecb21d290f
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-05 22:31:53 +0200

    Create Animal.java

---
+++ Java OOP/Inheritance/Exercises/ex06Animals/Animal.java
@@ -0,0 +1,58 @@
+package inheritance.exercises.ex06Animals;
+
+public class Animal {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Animal(String name, int age, String gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public void setName(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        if (gender.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid input!");
+        }
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public String toString() {
+
+        return getClass().getSimpleName() + System.lineSeparator() +
+                String.format("%s %d %s%n", name, age, gender) +
+                produceSound();
+    }
+
+    public String produceSound() {
+        return "";
+    }
+}
