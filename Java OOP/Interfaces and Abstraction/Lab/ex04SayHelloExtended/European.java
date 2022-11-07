c2b02ee6d62fd024a8eecdab6f13553ec529ec44
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-08 00:59:49 +0200

    Create European.java

---
+++ Java OOP/Interfaces and Abstraction/Lab/ex04SayHelloExtended/European.java
@@ -0,0 +1,13 @@
+package interfacesAndAbstraction.lab.ex04SayHelloExtended;
+
+public class European extends BasePerson {
+
+    public European(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+}
