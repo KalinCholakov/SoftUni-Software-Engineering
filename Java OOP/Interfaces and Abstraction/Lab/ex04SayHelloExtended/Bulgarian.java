f8d285ed02104eabfb934999be9adbbe13552239
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-08 00:59:46 +0200

    Create Bulgarian.java

---
+++ Java OOP/Interfaces and Abstraction/Lab/ex04SayHelloExtended/Bulgarian.java
@@ -0,0 +1,13 @@
+package interfacesAndAbstraction.lab.ex04SayHelloExtended;
+
+public class Bulgarian extends BasePerson {
+
+    public Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
