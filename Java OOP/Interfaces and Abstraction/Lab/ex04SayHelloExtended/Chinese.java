5c52ead8cf6c91cac7a3c3272d0923b469469543
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-08 00:59:53 +0200

    Create Chinese.java

---
+++ Java OOP/Interfaces and Abstraction/Lab/ex04SayHelloExtended/Chinese.java
@@ -0,0 +1,13 @@
+package interfacesAndAbstraction.lab.ex04SayHelloExtended;
+
+public class Chinese extends BasePerson {
+
+    public Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
