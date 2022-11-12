1bea01780dc70f0faa65e5271a943385bdb17f7a
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-12 22:58:23 +0200

    Create Soldier.java

---
+++ Java OOP/Interfaces and Abstraction/Exercises/ex06MilitaryElite/interfaces/Soldier.java
@@ -0,0 +1,10 @@
+package interfacesAndAbstraction.exercises.ex06MilitaryElite.interfaces;
+
+public interface Soldier {
+
+    int getId();
+
+    String getFirstName();
+
+    String getLastName();
+}
