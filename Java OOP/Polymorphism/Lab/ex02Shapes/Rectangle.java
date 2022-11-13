57034f5db8c293670d948439fd4ad133157b5ed5
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-13 11:49:50 +0200

    Create Rectangle.java

---
+++ Java OOP/Polymorphism/Lab/ex02Shapes/Rectangle.java
@@ -0,0 +1,21 @@
+package polymorphism.lab.ex02Shapes;
+
+public class Rectangle implements Shape {
+    private final Double height;
+    private final Double width;
+
+    public Rectangle(Double height, Double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public Double calculatePerimeter() {
+        return 2 * height + 2 * width;
+    }
+
+    @Override
+    public Double calculateArea() {
+        return height * width;
+    }
+}
