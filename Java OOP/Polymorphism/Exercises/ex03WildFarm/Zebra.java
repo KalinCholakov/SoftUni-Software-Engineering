3906e5018d52664006164c2b577d7cf8f54e93ac
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-14 15:27:29 +0200

    Create Zebra.java

---
+++ Java OOP/Polymorphism/Exercises/ex03WildFarm/Zebra.java
@@ -0,0 +1,22 @@
+package polymorphism.exercises.ex03WildFarm;
+
+public class Zebra extends Mammal {
+
+    public Zebra(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println("Zs");
+    }
+
+    @Override
+    void eatFood(Food food) {
+        if (food instanceof Vegetable) {
+            setFoodEaten(getFoodEaten() + food.getQuantity());
+        } else {
+            System.out.println("Zebras are not eating that type of food!");
+        }
+    }
+}
