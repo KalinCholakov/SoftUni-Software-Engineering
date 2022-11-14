78c84e858dc827446aeb58b0123afe1c6ebd877e
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-14 15:28:27 +0200

    Create Feline.java

---
+++ Java OOP/Polymorphism/Exercises/ex03WildFarmVar2/Feline.java
@@ -0,0 +1,8 @@
+package polymorphism.exercises.ex03WildFarmVar2;
+
+public abstract class Feline extends Mammal {
+
+    public Feline(String animalName, String animalType, double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+}
