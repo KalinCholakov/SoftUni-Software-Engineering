6917c38287215084f3ea68414e5e25560e93375b
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-14 15:28:11 +0200

    Create Animal.java

---
+++ Java OOP/Polymorphism/Exercises/ex03WildFarmVar2/Animal.java
@@ -0,0 +1,49 @@
+package polymorphism.exercises.ex03WildFarmVar2;
+
+public abstract class Animal {
+    private String animalName;
+    private String animalType;
+    private double animalWeight;
+    private int foodEaten;
+
+    public Animal(String animalName, String animalType, Double animalWeight) {
+        this.animalName = animalName;
+        this.animalType = animalType;
+        this.animalWeight = animalWeight;
+        this.foodEaten = 0;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public Double getAnimalWeight() {
+        return animalWeight;
+    }
+
+    public void setAnimalWeight(Double animalWeight) {
+        this.animalWeight = animalWeight;
+    }
+
+    public Integer getFoodEaten() {
+        return foodEaten;
+    }
+
+    abstract void makeSound();
+
+    public void eat(Food food) {
+        this.foodEaten += food.getQuantity();
+    }
+}
