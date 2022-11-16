ea37c8a276ea4d793bbb3eb6db7b90b7ebc09e66
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-16 12:39:35 +0200

    Create MrOperation.java

---
+++ Java OOP/Polymorphism/Exercises/ex05Calculator/MrOperation.java
@@ -0,0 +1,26 @@
+package polymorphism.exercises.ex05Calculator;
+
+import java.util.Deque;
+
+public class MrOperation implements Operation {
+    private Deque<Integer> memory;
+
+    public MrOperation(Deque<Integer> memory) {
+        this.memory = memory;
+    }
+
+    @Override
+    public void addOperand(int operand) {
+
+    }
+
+    @Override
+    public int getResult() {
+        return memory.pop();
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return true;
+    }
+}
