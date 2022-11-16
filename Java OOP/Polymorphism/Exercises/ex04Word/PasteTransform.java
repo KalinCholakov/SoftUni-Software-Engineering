ae687c3990fad661f0ceebe6dfffebfbbbfe4d09
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-16 11:49:50 +0200

    Create PasteTransform.java

---
+++ Java OOP/Polymorphism/Exercises/ex04Word/PasteTransform.java
@@ -0,0 +1,14 @@
+package polymorphism.exercises.ex04Word;
+
+public class PasteTransform implements TextTransform {
+    private CutTransform lastCut;
+
+    public PasteTransform(CutTransform lastCut) {
+        this.lastCut = lastCut;
+    }
+
+    @Override
+    public void invokeOn(StringBuilder text, int startIndex, int endIndex) {
+        text.replace(startIndex, endIndex, lastCut.getLastCut());
+    }
+}
