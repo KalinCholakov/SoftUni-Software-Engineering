13f616a63856bd9ecc5eb003e9d16192f8a39e41
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-16 12:39:09 +0200

    Create InputInterpreter.java

---
+++ Java OOP/Polymorphism/Exercises/ex05Calculator/InputInterpreter.java
@@ -0,0 +1,32 @@
+package polymorphism.exercises.ex05Calculator;
+
+public class InputInterpreter {
+    private CalculationEngine engine;
+
+    public InputInterpreter(CalculationEngine engine) {
+        this.engine = engine;
+    }
+
+    public boolean interpret(String input) {
+        try {
+            engine.pushNumber(Integer.parseInt(input));
+        } catch (Exception ex) {
+            engine.pushOperation(this.getOperation(input));
+        }
+        return true;
+    }
+
+    public Operation getOperation(String operation) {
+        if (operation.equals("*")) {
+            return new MultiplicationOperation();
+        } else if (operation.equals("/")) {
+            return new DivisionOperation();
+        } else if (operation.equals("ms")) {
+            return new MsOperation(engine.getMemory());
+        } else if (operation.equals("mr")) {
+            return new MrOperation(engine.getMemory());
+        }
+
+        return null;
+    }
+}
