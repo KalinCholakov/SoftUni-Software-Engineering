9dd5480ee680c9d6515981100028dab612ebf942
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-16 21:36:54 +0200

    Create Square.java

---
+++ Java OOP/SOLID/Lab/p03_LiskovSubstitution/p01_Square/Square.java
@@ -0,0 +1,28 @@
+package solidLab.p03_LiskovSubstitution.p01_Square;
+
+public class Square extends Rectangle {
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+    }
+}
