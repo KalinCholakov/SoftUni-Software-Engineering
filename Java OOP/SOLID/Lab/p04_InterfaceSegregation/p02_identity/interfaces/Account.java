6c210b59072906f9fca0219a375026f04240b288
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-16 23:11:36 +0200

    Create Account.java

---
+++ Java OOP/SOLID/Lab/p04_InterfaceSegregation/p02_identity/interfaces/Account.java
@@ -0,0 +1,16 @@
+package solidLab.p04_InterfaceSegregation.p02_identity.interfaces;
+
+public interface Account {
+
+    void register(String username, String password);
+
+    void login(String username, String password);
+
+    void changePassword(String oldPass, String newPass);
+
+    Iterable<User> getAllUsersOnline();
+
+    Iterable<User> getAllUsers();
+
+    User getUserByName(String name);
+}
