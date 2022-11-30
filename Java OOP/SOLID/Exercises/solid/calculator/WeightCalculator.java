925464f16f5ef025697c2e49ec06cc91a76b332e
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-30 21:42:12 +0200

    Create WeightCalculator.java

---
+++ Java OOP/SOLID/Exercises/solid/calculator/WeightCalculator.java
@@ -0,0 +1,18 @@
+package solid.calculator;
+
+import solid.products.Product;
+
+import java.util.List;
+
+public class WeightCalculator implements Calculator {
+
+    @Override
+    public double total(List<Product> products) {
+        return products.stream().mapToDouble(Product::getKilograms).sum();
+    }
+
+    @Override
+    public double average(List<Product> products) {
+        return total(products) / products.size();
+    }
+}
