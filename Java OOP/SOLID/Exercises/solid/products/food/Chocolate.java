809f23513c1858ced82183fa010c29de5b07aa12
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-11-30 21:41:42 +0200

    Create Chocolate.java

---
+++ Java OOP/SOLID/Exercises/solid/products/food/Chocolate.java
@@ -0,0 +1,10 @@
+package solid.products.food;
+
+public class Chocolate extends Food {
+
+    public static final double CALORIES_PER_100_GRAMS = 575.0;
+
+    public Chocolate(double grams, double caloriesPer100Grams) {
+        super(grams, caloriesPer100Grams);
+    }
+}
