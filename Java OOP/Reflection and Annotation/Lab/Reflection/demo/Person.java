ec658b62f949630e5ca8a5ec96e6cb45b031fbc3
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-12-06 17:30:13 +0200

    Create Person.java

---
+++ Java OOP/Reflection and Annotation/Lab/Reflection/demo/Person.java
@@ -0,0 +1,31 @@
+package reflection.demo;
+
+public class Person {
+    private final String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private Person(String name) {
+        this.name = name;
+    }
+
+    private final void run() {
+        System.out.println("Running");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
