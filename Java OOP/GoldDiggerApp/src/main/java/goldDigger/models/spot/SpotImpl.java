2f2f4caf5cc607eb50b84a0cc2e03a8c328dfab0
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-12-08 18:09:03 +0200

    Create SpotImpl.java

---
+++ Java OOP/GoldDiggerApp/src/main/java/goldDigger/models/spot/SpotImpl.java
@@ -0,0 +1,34 @@
+package goldDigger.models.spot;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static goldDigger.common.ExceptionMessages.SPOT_NAME_NULL_OR_EMPTY;
+
+public class SpotImpl implements Spot {
+    private String name;
+    private Collection<String> exhibits;
+
+    public SpotImpl(String name) {
+        this.setName(name);
+        this.exhibits = new ArrayList<>();
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new NullPointerException(SPOT_NAME_NULL_OR_EMPTY);
+        }
+
+        this.name = name;
+    }
+
+    @Override
+    public Collection<String> getExhibits() {
+        return exhibits;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
