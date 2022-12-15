7fb65e28eefe921ad65f921efc6dd471dde5f7db
Kalin Cholakov <cholakov_kalin@hotmail.com>
2022-12-15 22:41:57 +0200

    Create BaseCocktail.java

---
+++ Java OOP/ChristmasStoreApp/src/main/java/christmasPastryShop/entities/cocktails/BaseCocktail.java
@@ -0,0 +1,73 @@
+package christmasPastryShop.entities.cocktails;
+
+import christmasPastryShop.entities.cocktails.interfaces.Cocktail;
+
+import static christmasPastryShop.common.ExceptionMessages.*;
+
+public abstract class BaseCocktail implements Cocktail {
+    private String name;
+    private int size;
+    private double price;
+    private String brand;
+
+    protected BaseCocktail(String name, int size, double price, String brand) {
+        setName(name);
+        setSize(size);
+        setPrice(price);
+        setBrand(brand);
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException(INVALID_NAME);
+        }
+        this.name = name;
+    }
+
+    public void setSize(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException(INVALID_SIZE);
+        }
+        this.size = size;
+    }
+
+    public void setPrice(double price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException(INVALID_PRICE);
+        }
+        this.price = price;
+    }
+
+    public void setBrand(String brand) {
+        if (brand == null || brand.trim().isEmpty()) {
+            throw new IllegalArgumentException(INVALID_BRAND);
+        }
+        this.brand = brand;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getBrand() {
+        return brand;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s - %dml - %.2flv",
+                this.name, this.brand, this.size, this.price);
+    }
+}
